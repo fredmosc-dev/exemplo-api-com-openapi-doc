5845ad447496e9ad803a8b50213113562deddc50
Frederick Moschkowich <fm.webdeveloper@gmail.com>
2020-12-03 15:05:34 -0300

    Initial commit with swagger

--- src/main/java/dev/fredmosc/simpleapi/BibliotecaApplication.java
+++ src/main/java/dev/fredmosc/simpleapi/BibliotecaApplication.java
@@ -1,13 +1,27 @@
 package dev.fredmosc.simpleapi;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BibliotecaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BibliotecaApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Simple API").version(appVersion)
+                        .license(new License().name("MIT").url("http://localhost:8090/api/swagger.html")));
     }
 
 }
